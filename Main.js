6457bc596c4a49036706f370eeb9dda0bd565a0d
周明 <mmix1009@163.com>
2019-07-30 08:49:21 +0800

    [feature] 挂载离屏的逻辑

--- Main.js
+++ Main.js
@@ -1,51 +1,63 @@
-import { ResourceLoader } from '/js/base/ResourceLoader.js'
-import { Director } from '/js/director/Director.js'
-import { DataStore } from '/js/base/DataStore.js'
+import {ResourceLoader} from '/js/base/ResourceLoader.js'
+import {Director} from '/js/director/Director.js'
+import {DataStore} from '/js/base/DataStore.js'
 import {BackGround} from '/js/runtime/BackGround.js'
 import {Player} from "./js/player/Player.js"
 import {Enemy} from "./js/npc/Enemy.js"
 import {GameOver} from "./js/runtime/GameOver.js"
 import {FirstDirector} from "./js/director/FirstDirector";
-import  {Music} from "./js/runtime/Music";
+import {Music} from "./js/runtime/Music";
 
 
 const EMEMYCOUNT = 20;
 
 // 开始类
 export class Main {
-  constructor(){
+    constructor() {
 
-    /**
-     * 挂载全局的分辨率和适配分辨率的方法，其他类不用引用dataStore直接进行适配,而且不用暴露实现细节，方便后期修改
-     */
-    GameGlobal.dpr = wx.getSystemInfoSync().pixelRatio;
-    GameGlobal.fit = function(x) {
-        return GameGlobal.dpr * x;
+        this.configGameGlobal();
+        this.res = wx.getSystemInfoSync();
+        //创建资源文件加载器
+        this.canvas = GameGlobal.canvas;
+        this.canvas.height = GameGlobal.fit(this.canvas.height);
+        this.canvas.width = GameGlobal.fit(this.canvas.width);
+        const loader = ResourceLoader.create();
+        loader.onLoaded((map, ziku) => this.onResourceFirstLoaded(map, ziku));
+
+        wx.onShow((object) => {
+            Music.getInstance().playBGM();
+        })
     }
-    this.res = wx.getSystemInfoSync();
-    //创建资源文件加载器
-    this.canvas = wx.createCanvas();
-    this.canvas.height = GameGlobal.fit(this.canvas.height);
-    this.canvas.width = GameGlobal.fit(this.canvas.width);
-    const loader = ResourceLoader.create();
-    loader.onLoaded((map,ziku) => this.onResourceFirstLoaded(map, ziku));
 
-    wx.onShow((object)=>{
-      Music.getInstance().playBGM();
-    })
-  }
+    // 所有资源加载完毕后才能渲染
+    onResourceFirstLoaded(map, ziku) {
+        // 将画笔和画布放在dataStore方便精灵使用
+        DataStore.getInstance().systeminfo = this.res;
+        DataStore.getInstance().res = map;
+        DataStore.getInstance().canvas = this.canvas;
+        DataStore.getInstance().ctx = this.canvas.getContext('2d');
+        let object = JSON.parse(ziku);
+        DataStore.getInstance().ziku = object;
+        this.director = new Director;
+    }
 
-  // 所有资源加载完毕后才能渲染
-  onResourceFirstLoaded(map,ziku) {
-    // 将画笔和画布放在dataStore方便精灵使用
-    DataStore.getInstance().systeminfo=this.res;
-    DataStore.getInstance().res = map;
-    DataStore.getInstance().canvas = this.canvas;
-    DataStore.getInstance().ctx = this.canvas.getContext('2d');
-    let object = JSON.parse(ziku);
-    DataStore.getInstance().ziku = object;
-    this.director = new Director;
-  }
+    configGameGlobal() {
+        /**
+         * 挂载全局的分辨率和适配分辨率的方法，其他类不用引用dataStore直接进行适配,而且不用暴露实现细节，方便后期修改
+         */
+        GameGlobal.dpr = wx.getSystemInfoSync().pixelRatio;
+        GameGlobal.fit = function (x) {
+            return GameGlobal.dpr * x;
+        }
+        GameGlobal.canvas = wx.createCanvas();
+        GameGlobal.isOffScreen = function (x, y) {
+            if (x < 0 || x > GameGlobal.width || y < 0 || y > GameGlobal.height) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 
 
 }
